40f357368626f1847835df87b688ec85b9b696e7
Nakul Rajpal <66713174+Nakul-Rajpal@users.noreply.github.com>
2025-06-25 13:58:55 -0500

    Data_Processing1
    Added basic data processing of initial dataset + basic model (not setup)

--- data.py
+++ data.py
@@ -1,3 +1,31 @@
 import pandas as pd
+import glob
+import os
+from collections import defaultdict
 
-df = pd.read_csv("conn.log.labeled",sep='\t',comment='#',low_memory=False)
+data_dir = 'data'
+parquet_files = glob.glob(os.path.join(data_dir, '*.parquet'))
+grouped_dfs = defaultdict(list)
+for filepath in parquet_files:
+    filename = os.path.basename(filepath)
+    base = filename.split('.')[0].split('_')[0]
+    grouped_dfs[base].append(filepath)
+
+combined_datasets = {}
+
+if 'part' in grouped_dfs:
+    part_dfs = [pd.read_parquet(f) for f in sorted(grouped_dfs['part'])]
+    combined_datasets['part'] = pd.concat(part_dfs, ignore_index=True)
+for base, files in grouped_dfs.items():
+    if base == 'part':
+        continue
+    dfs = [pd.read_parquet(f) for f in sorted(files)]
+    combined_datasets[base] = pd.concat(dfs, ignore_index=True)
+
+final_df = pd.concat(combined_datasets.values(), ignore_index=True)
+print(f"\nFinal combined shape: {final_df.shape}")
+
+
+print(combined_datasets)
+
+
