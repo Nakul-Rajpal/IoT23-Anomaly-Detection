67e9e21817cbe04c2138d5108195bbc3c88155d4
Nakul Rajpal <66713174+Nakul-Rajpal@users.noreply.github.com>
2025-07-02 13:59:13 -0500

    Update data
    Removed preprocessing + changed to jupyter notebook

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 from model import Autoencoder
 
-INPUT_SIZE = 6
+INPUT_SIZE = 18
 MODEL1 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[16, 8])
 MODEL2 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[32, 16, 8])
 MODEL3 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[64, 32, 16, 8])
