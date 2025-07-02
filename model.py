e537b0957acbb3e60108c056143e2149d4420bcb
Nakul Rajpal <66713174+Nakul-Rajpal@users.noreply.github.com>
2025-07-02 16:35:22 -0500

    Added small changes to data + training
    Modified Loss function, updated model/training

--- model.py
+++ model.py
@@ -13,17 +13,22 @@ class Autoencoder(nn.Module):
             input_dim = h
         self.encoder = nn.Sequential(*encoder_layers)
 
+        self.bottleneck = nn.Linear(input_dim, 4)
+        input_dim = 4
+
         decoder_layers = []
         for h in reversed(hidden_dims[:-1]):
             decoder_layers.append(nn.Linear(input_dim, h))
             decoder_layers.append(nn.ReLU())
             input_dim = h
         decoder_layers.append(nn.Linear(input_dim, self.original_input_dim))  
+        decoder_layers.append(nn.Sigmoid()) 
         self.decoder = nn.Sequential(*decoder_layers)
 
     def forward(self, x):
         encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
+        bottlenecked = self.bottleneck(encoded)
+        decoded = self.decoder(bottlenecked)
         return decoded
 
 
