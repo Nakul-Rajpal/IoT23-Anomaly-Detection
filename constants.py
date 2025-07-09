3c7d47242c613a108c5b78d70e0e680a81da5121
Nakul Rajpal <66713174+Nakul-Rajpal@users.noreply.github.com>
2025-07-09 13:45:57 -0500

    Modified loss + threshold function

--- constants.py
+++ constants.py
@@ -2,5 +2,6 @@ from model import Autoencoder
 
 INPUT_SIZE = 18
 MODEL1 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[16, 8])
-MODEL2 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[32, 16, 8])
+MODEL2 = Autoencoder(input_dim=18, hidden_dims=[16, 8, 4])
+MODEL4 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[32, 16, 8])
 MODEL3 = Autoencoder(input_dim=INPUT_SIZE, hidden_dims=[64, 32, 16, 8])
--- model.py
+++ model.py
@@ -2,33 +2,67 @@ import torch.nn as nn
 import torch
 
 class Autoencoder(nn.Module):
-    def __init__(self, input_dim, hidden_dims):
+    def __init__(self, input_dim):
         super(Autoencoder, self).__init__()
-        self.original_input_dim = input_dim 
 
-        encoder_layers = []
-        for h in hidden_dims:
-            encoder_layers.append(nn.Linear(input_dim, h))
-            encoder_layers.append(nn.ReLU())
-            input_dim = h
-        self.encoder = nn.Sequential(*encoder_layers)
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+            nn.ReLU()
+        )
 
-        self.bottleneck = nn.Linear(input_dim, 4)
-        input_dim = 4
+        # Bottleneck layer 
+        self.bottleneck = nn.Linear(4, 4)
 
-        decoder_layers = []
-        for h in reversed(hidden_dims[:-1]):
-            decoder_layers.append(nn.Linear(input_dim, h))
-            decoder_layers.append(nn.ReLU())
-            input_dim = h
-        decoder_layers.append(nn.Linear(input_dim, self.original_input_dim))  
-        decoder_layers.append(nn.Sigmoid()) 
-        self.decoder = nn.Sequential(*decoder_layers)
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(4, 8),
+            nn.ReLU(),
+            nn.Linear(8, 16),
+            nn.ReLU(),
+            nn.Linear(16, input_dim),
+            nn.Sigmoid()  
+        )
 
     def forward(self, x):
-        encoded = self.encoder(x)
-        bottlenecked = self.bottleneck(encoded)
-        decoded = self.decoder(bottlenecked)
-        return decoded
+        x = self.encoder(x)
+        x = self.bottleneck(x)
+        x = self.decoder(x)
+        return x
 
+# import torch.nn as nn
+# import torch
 
+# class Autoencoder(nn.Module):
+#     def __init__(self, input_dim, hidden_dims):
+#         super(Autoencoder, self).__init__()
+#         self.original_input_dim = input_dim 
+
+#         encoder_layers = []
+#         for h in hidden_dims:
+#             encoder_layers.append(nn.Linear(input_dim, h))
+#             encoder_layers.append(nn.ReLU())
+#             input_dim = h
+#         self.encoder = nn.Sequential(*encoder_layers)
+
+#         self.bottleneck = nn.Linear(input_dim, 4)
+#         input_dim = 4
+
+#         decoder_layers = []
+#         for h in reversed(hidden_dims[:-1]):
+#             decoder_layers.append(nn.Linear(input_dim, h))
+#             decoder_layers.append(nn.ReLU())
+#             input_dim = h
+#         decoder_layers.append(nn.Linear(input_dim, self.original_input_dim))  
+#         decoder_layers.append(nn.Sigmoid()) 
+#         self.decoder = nn.Sequential(*decoder_layers)
+
+#     def forward(self, x):
+#         encoded = self.encoder(x)
+#         bottlenecked = self.bottleneck(encoded)
+#         decoded = self.decoder(bottlenecked)
+#         return decoded
